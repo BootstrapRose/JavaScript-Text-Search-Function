63f864a6efb2fefcfc3125a130e3ab3657ed6358
Rose <aerwings@gmail.com>
2015-08-15 17:24:46 -0400

    Update search.js

--- search.js
+++ search.js
@@ -1,12 +1,12 @@
 jshint multistr:true
 
 var text = "skdfjdsf ROse sdlkfjsdkfj \ ROSE dskfjdskf Rose KJDFKLJDSF \ kdsjfkd fjjfsj ksksk rose?";
-myName = "Rose";
-hits=[];
+var myName = "Rose";
+var hits=[];
 
 for (i=0; i < text.length; i++){
     if (myName[0] === text[i]){
-        for (var j = 0; j < myName.length; j++){
+        for (var i = 0; i < myName.length; i++){
             hits.push('Rose');
         }
     }
