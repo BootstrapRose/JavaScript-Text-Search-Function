bf13879169d71f62f9dcd9deb81a8fd8cd713777
Rose <aerwings@gmail.com>
2015-08-15 17:37:15 -0400

    Update search.js

--- search.js
+++ search.js
@@ -4,10 +4,10 @@ var text = "skdfjdsf ROse sdlkfjsdkfj \ ROSE dskfjdskf Rose KJDFKLJDSF \ kdsjfkd
 var myName = "Rose";
 var hits=[];
 
-for (i=0; i < text.length; i++){
+for (i = 0; i < text.length; i++) {
     if (myName[0] === text[i]){
-        for (var i = 0; i < myName.length; i++){
-            hits.push('Rose');
+        for (var j = i; j < (myName.length + i); j++){
+            hits.push(text[j]);
         }
     }
 }
